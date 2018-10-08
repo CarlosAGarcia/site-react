91a6aef8f00ead2226c77ac016db0e7611072b9e
CarlosAGarcia <thethirdcage@gmail.com>
2018-10-09 02:57:14 +1100

    npm redux installations

--- src/App.js
+++ src/App.js
@@ -5,36 +5,19 @@ import {createStore} from 'redux';
 class App extends Component {
   render() {
     
-    //reducer counter takes past state, action and gives new state
-    //never touch this const or aloter it
-    const counter = (state = 0, action) => {
-      switch (action.type){
-        case 'INCREASE' :
-          return state+1;
-        case 'DECREASE' :
-          return state-1;
-        default:
-          return state;
-      }
-    }
 
-    const store = createStore(counter);
-    console.log("state is "+store.getState());
 
-    store.dispatch({type:'INCREASE'});
-    console.log("state is "+store.getState());
 
-    const render2 = ()=> store.getState();
-     store.subscribe(render2);
-     
-     document.addEventListener('click', ()=>{store.dispatch({type: 'INCREASE'})});
-    
+
+
+
+
 
     return (
 
       
       <div className="App">
-        {render2}
+        {}
 
       </div>
     );
